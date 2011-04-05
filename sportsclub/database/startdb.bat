2e6d48c58f5c9d510dda9afa74f818c4af5c60ae
Marius Bogoevici <marius.bogoevici@gmail.com>
2011-04-04 21:32:29 -0400

    JBPAPP-4801 fix Windows script

--- sportsclub/database/startdb.bat
+++ sportsclub/database/startdb.bat
@@ -1,8 +1,7 @@
 if "x%HSQLDB_JAR_LOCATION%" == "x" (
    if "x%HSQLDB_HOME%" == "x" (
      HSQLDB_JAR_LOCATION=%USERPROFILE%/.m2/repository/hsqldb/hsqldb/1.8.0.10/hsqldb-1.8.0.10.jar
-   )
-   else (
+   ) else (
      HSQLDB_JAR_LOCATION=%HSQLDB_HOME%/lib/hsqldb.jar
    )
 )
